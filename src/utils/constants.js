1e9afd698d3d4c639e7e48bf95d62591f0488d4e
rohan <admin@Admins-MacBook-Pro.local>
2023-11-18 00:09:12 -0500

    wip

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:8800/api/'
+export const BASE_URL = 'https://backend-unipeat.onrender.com/api/'
 export const GB_CURRENCY = Intl.NumberFormat('en-GB', {
   style: 'currency',
   currency: 'GBP',
